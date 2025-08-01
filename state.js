ce797434222768e5963e1f27f6cbb96435ec37d3
Aris Zulwafa <119752803+andrianSyio@users.noreply.github.com>
2025-08-01 11:23:09 +0700

    Update state.js

--- state.js
+++ state.js
@@ -1,6 +1,6 @@
 import allSoal from './data';
 
-let soalAcak = allSoal.sort(() => Math.random() - 0.5); // acak semua soal
+let soalAcak = allSoal.sort(() => Math.random() - 0.5); // acak urutan soal
 
 let state = {
   soalIndex: 0,
