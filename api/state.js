1ce257b602ba0cad7a6386e397c4e5b1ad13da4f
Aris Zulwafa <119752803+andrianSyio@users.noreply.github.com>
2025-08-01 11:17:10 +0700

    Update state.js

--- api/state.js
+++ api/state.js
@@ -1,9 +1,1 @@
-// /api/state.js
-import { soalSekarang, terjawab } from './data.js';
 
-export default function handler(req, res) {
-  res.status(200).json({
-    soal: soalSekarang(),
-    terjawab
-  });
-}
