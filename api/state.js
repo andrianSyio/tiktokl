9bc15ca8004f51f1408c5d7a72c3871431468490
Aris Zulwafa <119752803+andrianSyio@users.noreply.github.com>
2025-08-01 11:28:38 +0700

    Update state.js

--- api/state.js
+++ api/state.js
@@ -1,9 +1,9 @@
-// file: /api/state.js
-import { state } from '../../state';
+// /api/state.js
+import { state } from "../../state.js";
 
 export default function handler(req, res) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
   }
 
   const currentSoal = state.soalList[state.soalIndex];
