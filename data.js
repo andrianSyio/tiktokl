b8311726d422494f7726966e84ae952f76bb7e31
Aris Zulwafa <119752803+andrianSyio@users.noreply.github.com>
2025-08-01 11:00:32 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,82 @@
+export const soalList = [
+  {
+    soal: "Sebutkan alat mandi!",
+    jawaban: ["sabun", "sikat gigi", "odol", "shampoo", "gayung", "handuk"]
+  },
+  {
+    soal: "Sebutkan hewan berkaki empat!",
+    jawaban: ["kucing", "anjing", "sapi", "kerbau", "kambing", "harimau"]
+  },
+  {
+    soal: "Sebutkan buah berwarna merah!",
+    jawaban: ["apel", "stroberi", "semangka", "ceri", "delima", "tomat"]
+  },
+  {
+    soal: "Sebutkan aplikasi chatting!",
+    jawaban: ["whatsapp", "telegram", "line", "messenger", "wechat", "signal"]
+  },
+  {
+    soal: "Sebutkan kendaraan di jalan!",
+    jawaban: ["mobil", "motor", "bus", "truk", "sepeda", "angkot"]
+  },
+  {
+    soal: "Sebutkan warna pelangi!",
+    jawaban: ["merah", "jingga", "kuning", "hijau", "biru", "ungu"]
+  },
+  {
+    soal: "Sebutkan makanan Indonesia!",
+    jawaban: ["nasi goreng", "rendang", "sate", "bakso", "gado-gado", "soto"]
+  },
+  {
+    soal: "Sebutkan anggota tubuh manusia!",
+    jawaban: ["mata", "telinga", "hidung", "mulut", "tangan", "kaki"]
+  },
+  {
+    soal: "Sebutkan musim dalam setahun!",
+    jawaban: ["hujan", "kemarau", "semi", "panas", "gugur", "dingin"]
+  },
+  {
+    soal: "Sebutkan media sosial populer!",
+    jawaban: ["tiktok", "instagram", "facebook", "x", "threads", "youtube"]
+  },
+  {
+    soal: "Sebutkan jenis pekerjaan!",
+    jawaban: ["dokter", "guru", "petani", "polisi", "pilot", "programmer"]
+  },
+  {
+    soal: "Sebutkan nama hari!",
+    jawaban: ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu"]
+  },
+  {
+    soal: "Sebutkan jenis olahraga!",
+    jawaban: ["sepak bola", "basket", "voli", "renang", "badminton", "lari"]
+  },
+  {
+    soal: "Sebutkan alat sekolah!",
+    jawaban: ["penghapus", "buku", "pensil", "pulpen", "penggaris", "tas"]
+  },
+  {
+    soal: "Sebutkan jenis minuman!",
+    jawaban: ["teh", "kopi", "air", "jus", "susu", "soda"]
+  },
+  {
+    soal: "Sebutkan planet di tata surya!",
+    jawaban: ["merkurius", "venus", "bumi", "mars", "jupiter", "saturnus"]
+  },
+  {
+    soal: "Sebutkan hewan laut!",
+    jawaban: ["ikan", "hiu", "paus", "ubur-ubur", "kuda laut", "gurita"]
+  },
+  {
+    soal: "Sebutkan benda di dapur!",
+    jawaban: ["kompor", "panci", "wajan", "sendok", "pisau", "gelas"]
+  },
+  {
+    soal: "Sebutkan bahan makanan pokok!",
+    jawaban: ["beras", "jagung", "ubi", "sagu", "kentang", "roti"]
+  },
+  {
+    soal: "Sebutkan aktivitas di pagi hari!",
+    jawaban: ["bangun tidur", "mandi", "sarapan", "gosok gigi", "olahraga", "minum kopi"]
+  }
+];
